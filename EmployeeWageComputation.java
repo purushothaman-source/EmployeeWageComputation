a62ff9c9f89133586b231a19089085a5a24cb715
purushothaman-source <purushoth99jun@gmail.com>
2020-11-24 11:11:33 +0530

    ADDED :Welcome message

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.day21.employeeWageComputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Problem"); }
+
+}
